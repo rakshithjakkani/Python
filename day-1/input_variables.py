3ddec6c9e43780c3474d8595f54c9ce89c25a3c4
Rakshith Jakkani <rakshithjakkani@fynd-external.com>
2024-09-17 17:07:59 +0530

    learning day-1 has added

---
+++ day-1/input_variables.py
@@ -0,0 +1,10 @@
+#input variables code
+print("Hi " + input("Hello whats your name\n: ") + "!")
+
+
+# variable defining and declaration
+
+name = input("Hey what your name\n:")
+age = int(input("Whats your age\n:"))
+
+print(f'Here is your information, please check\nName={name}\nAge={age}')
---
+++ day-1/printing.py
@@ -0,0 +1,12 @@
+print("Hello rakshith!") # This prints a single line 
+
+print("Hello rakshith!\nHello rakshith!") # \n used for to print another sentence on the next line
+
+
+# string concatinating, which means that adding two strings as below code
+print("Hello" + "there")
+print("Hello" + " " + "there")
+
+# the below we call f string
+my_girl = "suma"
+print(f'hello hi {my_girl}')
