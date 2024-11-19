c953ab6c88924c3e1b7a33e9c3a00b13a4d91cfd
Rakshith Jakkani <rakshithjakkani@fynd-external.com>
2024-11-20 03:11:23 +0530

    added conditions

---
+++ 3-conditions-operators/even-odd.py
@@ -0,0 +1,7 @@
+print("Welcome to even odd number checker")
+number_to_check = int(input("provide me the number to check: "))
+
+if number_to_check % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
---
+++ 3-conditions-operators/nested-if-elif-else.py
@@ -0,0 +1,21 @@
+print("Hi welcome to rollercoaster")
+height = int(input("what is your height in cm ?: "))
+bill = 0
+
+if height >= 120:
+    print("you can ride a rollercoaster")
+    age = int(input("what is your age ?: "))
+    if age < 12:
+        bill = 7
+    elif age >= 12 and age < 18:
+        bill = 12
+    else: 
+        bill = 20
+    
+    photo = input("do you want a picture on rollercoaster ? if yes, type y if not type n: ")
+    if photo == "y":
+        bill += 3
+    print(f"your final bill would be ${bill}")
+
+else:
+    print("sorry you need to grow up to ride a rollercoaster")
