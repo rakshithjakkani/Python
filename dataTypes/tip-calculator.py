f560639b79df519ec4c8da320b3ba4057fdd439f
Rakshith Jakkani <rakshithjakkani@fynd-external.com>
2024-11-19 12:52:21 +0530

    updated tip-calculator.py

--- dataTypes/tip-calculator.py
+++ dataTypes/tip-calculator.py
@@ -1,20 +1,19 @@
-print("Welcome to JAKKANI's restuarent. here is your bill $128.50")
+print("Welcome to JAKKANI's restuarent")
 
-bill = 128.50
+
+bill = float(input("what was the bill ?: $"))
 tip = int(input("how much percentage you want to give tip ?: "))
 
-tip /= 100
-total_tip = tip * bill
-total_bill = bill + total_tip
+total_bill = tip / 100 * bill + bill
 actual_bill = round(total_bill, 2)
-print(f"this is your total bill sir {actual_bill}")
+print(f"this is your total bill sir ${actual_bill}")
 
-spplit_bool = input("do you want to split it sir ?")
+spplit_bool = input("do you want to split it sir ?: ")
 
 if spplit_bool == "yes":
     split_number = int(input("among how many members sir ?"))
     final_bill = actual_bill / split_number
-    print(f"this is your split amount: {final_bill}")
+    print(f"this is your splitted amount: ${final_bill}")
 
 else:
-    print(f"please play amount {actual_bill}")
+    print(f"please play amount ${actual_bill}")
