ddd589bb19a969f9b2465d64dca886664a390857
Rakshith Jakkani <rakshithjakkani@fynd-external.com>
2024-11-20 03:12:35 +0530

    updated

---
+++ 1-day/Bandname_generator.py
@@ -0,0 +1,7 @@
+print("welcome to name gernator!")
+city_name = input("what is your city name\n:")
+pet_name = input("what is your pet name\n:")
+
+print("Here is your band name: ", city_name + " " + pet_name)
+
+# print(f"Here is your band name: {city_name} {pet_name}")
---
+++ 1-day/input_variables.py
@@ -0,0 +1,12 @@
+# input variables code
+print("Hi " + input("Hello whats your name\n: ") + "!")
+
+
+
+# # variable defining and declaration
+
+name = input("Hey what your name\n:")
+age = int(input("Whats your age\n:"))
+name_length = len(name)
+
+print(f'Here is your information, please check\nName=\{name}\nAge={age}\nLength of your name = {name_length}')
