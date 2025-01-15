7b47a7d6b0ab0840dd129d68cc7d5ee6f51e4263
Rakshith Jakkani <rakshithjakkani@fynd-external.com>
2025-01-15 15:30:06 +0530

    updated python repo

---
+++ 13-error-handling/try-except.py
@@ -0,0 +1,12 @@
+# def age():
+#     try:
+#         age = int(input("what is your age?: "))
+#     except ValueError:
+#         print("You entered an incorrect number. Please try again with correct value. Example 1, 2, ... n")
+#         age = int(input("what is your age?: "))
+#     if age >= 20:
+#         print(f"Hey thanks for supporting us. This is your age {age}")
+#     else:
+#         print("Hey kid thanks for testing this.")
+# age()
+
---
+++ 14-Higher-lower-game/game_data.py
@@ -0,0 +1,30 @@
+data = [{
+    'name': 'rakshith jakkani',
+    'follower_count': 700,
+    'description': 'A normal devops engineer who is writing this code',
+    'city': 'Mumbai'
+},
+{
+    'name': 'Ritesh Kumar',
+    'follower_count': 600,
+    'description': 'A normal devops engineer who is working in opstree solutions',
+    'city': 'Mumbai'
+},
+{
+    'name': 'Hemanth',
+    'follower_count': 720,
+    'description': 'A normal devops engineer who is working in zeta',
+    'city': 'Hyderabad'
+},
+{
+    'name': 'Bharath Kumar',
+    'follower_count': 900,
+    'description': 'A fully qualified DevOps engineer whos is working in Minfy',
+    'city': 'Hyderabad'
+},
+{
+    'name': 'Priyanka Shindey',
+    'follower_count': 702,
+    'description': 'A cloud engineer who is working in Infosys',
+    'city': 'Bangalore'
+}]
