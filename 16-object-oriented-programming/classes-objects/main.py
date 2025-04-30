8ca9a7ba26a2876dee9bfee4cd6fca7840edfd67
rakshithjakkaniminfytech <rakshit.jakkani@minfytech.com>
2025-04-30 17:49:06 +0530

    updated

---
+++ 16-object-oriented-programming/classes-objects/main.py
@@ -0,0 +1,20 @@
+# import turtle
+# import rakshith
+# print(rakshith.rakshith)
+
+# from turtle import Turtle, Screen
+
+# timmy = Turtle()
+# my_screen = Screen()
+# timmy.shape("turtle")
+# timmy.color("red")
+# timmy.forward(200)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Mumbai Places", ["merin drive", "zoho beach", "colaba stree"])
+table.add_column("Mumbai Restaurents", ["Colaba", "Veronika's", "Foodies"])
+table.align = 'l'
+table.sortby = "Mumbai Places"
+print(table)
